9c1e882ed38b80572a631c0cb488a7b0364f658b
icanp1ay <262359709@qq.com>
2022-09-23 18:32:18 +0800

    Add files via upload

---
+++ 9.23.cpp
@@ -0,0 +1,7 @@
+https://mp.weixin.qq.com/s/E_n7nGx_zcokwbm1aglA-g
+
+https://haiderm.com/undetectable-backdooring-pe-file/
+
+https://gist.github.com/X-C3LL/ba905b10163f769061ce619f26c138b8
+
+https://codewhitesec.blogspot.com/2022/09/attacks-on-sysmon-revisited-sysmonente.html
